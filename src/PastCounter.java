10c997ff6ef2c46ee9a3f5b7240a105a0c3accdc
Fluoclip <59053824+Fluoclip@users.noreply.github.com>
2021-10-15 09:26:17 +0200

    Adding counting the shiffts in the past.

---
+++ src/PastCounter.java
@@ -0,0 +1,13 @@
+
+public class PastCounter {
+	
+	static int employees = TwoDimArray.employees;
+	static int [] pastCounter = new int[employees];
+	static int counterInArray = 0;
+	
+	
+	
+	
+	
+
+}
